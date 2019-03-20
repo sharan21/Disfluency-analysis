8c4d8d06b2860c97f5f4f206df3b7b8472cc0f2e
ercagarwal <sharan.n21@gmail.com>
2019-03-20 22:23:46 +0530

    mod

--- speech-monitor.py
+++ speech-monitor.py
@@ -12,13 +12,10 @@ from pydub.silence import split_on_silence
 from utils import getNumberOfFiles
 
 
-chunk_duration = 0.5 # Each read length in seconds from mic.
+chunk_duration = 0.15 # Each read length in seconds from mic.
 fs = 44100 # sampling rate for mic
 chunk_samples = int(fs * chunk_duration) # Each read length in number of samples.
 
-# Each model input data duration in seconds, need to be an integer numbers of chunk_duration
-feed_duration = 10 # the input size for the model
-feed_samples = int(fs * feed_duration)
 DEFAULT_CHUNKNAME = './chunks/chunk{}.wav'
 
 frames = []
@@ -26,9 +23,7 @@ writing = False
 fileOffset = getNumberOfFiles()
 
 
-assert feed_duration/chunk_duration == int(feed_duration/chunk_duration)
-
-model = load_model('./models/tr_model.h5')
+# model = load_model('./models/tr_model.h5')
 
 
 def get_audio_input_stream(callback):
@@ -42,92 +37,6 @@ def get_audio_input_stream(callback):
         stream_callback=callback)
     return stream
 
-
-def get_spectrogram(data):
-    """
-    Function to compute a spectrogram.
-
-    Argument:
-    predictions -- one channel / dual channel audio data as numpy array
-
-    Returns:
-    pxx -- spectrogram, 2-D array, columns are the periodograms of successive segments.
-    """
-    nfft = 200  # Length of each window segment
-    fs = 8000  # Sampling frequencies
-    noverlap = 120  # Overlap between windows
-    nchannels = data.ndim
-    if nchannels == 1:
-        pxx, _, _ = mlab.specgram(data, nfft, fs, noverlap=noverlap)
-    elif nchannels == 2:
-        pxx, _, _ = mlab.specgram(data[:, 0], nfft, fs, noverlap=noverlap)
-    return pxx
-
-
-def plt_spectrogram(data):
-    """
-    Function to compute and plot a spectrogram.
-
-    Argument:
-    predictions -- one channel / dual channel audio data as numpy array
-
-    Returns:
-    pxx -- spectrogram, 2-D array, columns are the periodograms of successive segments.
-    """
-    nfft = 200  # Length of each window segment
-    fs = 8000  # Sampling frequencies
-    noverlap = 120  # Overlap between windows
-    nchannels = data.ndim
-    if nchannels == 1:
-        pxx, _, _, _ = plt.specgram(data, nfft, fs, noverlap=noverlap)
-    elif nchannels == 2:
-        pxx, _, _, _ = plt.specgram(data[:, 0], nfft, fs, noverlap=noverlap)
-    return pxx
-
-
-def detect_triggerword_spectrum(x):
-    """
-    Function to predict the location of the trigger word.
-
-    Argument:
-    x -- spectrum of shape (freqs, Tx)
-    i.e. (Number of frequencies, The number time steps)
-
-    Returns:
-    predictions -- flattened numpy array to shape (number of output time steps)
-    """
-    # the spectogram outputs  and we want (Tx, freqs) to input into the model
-    x = x.swapaxes(0, 1)
-    x = np.expand_dims(x, axis=0)
-    predictions = model.predict(x)
-    return predictions.reshape(-1)
-
-
-def has_new_triggerword(predictions, chunk_duration, feed_duration, threshold=0.5):
-    """
-    Function to detect new trigger word in the latest chunk of input audio.
-    It is looking for the rising edge of the predictions data belongs to the
-    last/latest chunk.
-
-    Argument:
-    predictions -- predicted labels from model
-    chunk_duration -- time in second of a chunk
-    feed_duration -- time in second of the input to model
-    threshold -- threshold for probability above a certain to be considered positive
-
-    Returns:
-    True if new trigger word detected in the latest chunk
-    """
-    predictions = predictions > threshold
-    chunk_predictions_samples = int(len(predictions) * chunk_duration / feed_duration)
-    chunk_predictions = predictions[-chunk_predictions_samples:]
-    level = chunk_predictions[0]
-    for pred in chunk_predictions:
-        if pred > level:
-            return True
-        else:
-            level = pred
-    return False
 
 
 
@@ -189,25 +98,10 @@ def callback(in_data, frame_count, time_info, status): # also responsible for pu
         # print('writing')
         sys.stdout.write('.')
         frames.append(in_data)
-
-
         writing = True
 
 
-
     # splitWavFileAndStore(filename)
-
-
-
-    # if len(data) > feed_samples: # this line only runs when there is fresh data greater than 10 seconds
-    #
-    #     data = data[-feed_samples:]
-    #
-    #
-    #
-    #
-    #     # Process data async by sending a queue.
-    #     q.put(data)
 
     return (in_data, pyaudio.paContinue)
 
@@ -215,17 +109,17 @@ def callback(in_data, frame_count, time_info, status): # also responsible for pu
 if __name__ == '__main__':
 
     # Queue to communicate between the audio callback and main thread
-    q = Queue()
+
+    # q = Queue()
 
     run = True
 
     silence_threshold = 100  # not in db
 
     # Run the demo for a timeout seconds
-    timeout = time.time() + 0.5 * 60  # 0.5 minutes from now
+    timeout = time.time() + 2 * 60  # 2 minutes from now
 
     # Data buffer for the input wavform
-    data = np.zeros(feed_samples, dtype='int16')
 
     stream = get_audio_input_stream(callback)  # creates the PyAudio() instance with callback function
     # every time
@@ -234,9 +128,9 @@ if __name__ == '__main__':
     try:
         while run:
 
-            data = q.get()  # FIFO
+            continue
 
-            print(run)
+            # data = q.get()  # FIFO
 
             # # he uses spectrum data to make preds
             # spectrum = get_spectrogram(data)
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import wave
 import numpy as np
 import math
 import os
+import scipy
+
+# these constants are used to simplify specifying a default argument for below functions while importing
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -12,6 +15,29 @@ RECORD_SECONDS = 3
 status = True
 
 p = pyaudio.PyAudio()
+
+def startRecording(seconds = RECORD_SECONDS):
+    frames = []
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+    print("* recording")
+
+    for i in range(0, int(RATE / CHUNK * seconds)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    return frames
+
 
 
 
@@ -46,7 +72,44 @@ def getNumberOfFiles(path = './samples'):
     # print ("Number of Files found in all_chunks:", number_files-1)
     return number_files
 
+def detectnoiselevel(seconds = 5): # in dBFS
+
+    print ("detecting noise level...")
+    print ("recording for {} seconds".format(seconds))
+
+    frames = startRecording(seconds)
+    storeWavFile(frames, './chunks')
+
+    data = importAllFromDir('./temp')
+
+    print (20*math.log10(np.mean(data)/32767))
+
+
+def importAllFromDir(path):
+
+    list = os.listdir(path)
+
+    if '.DS_Store' in list:
+        list.remove('.DS_Store')
+
+    print ("There are {} chunks in directory {}".format(len(list), path))
+
+    sounddata = []
+
+    for i in range(len(list)):
+
+        rate, data = scipy.io.wavfile.read(path+'/'+list[i])
+
+        # rectifies sound signal
+        sounddata.append(np.absolute(data))
+
+    return sounddata
+
+
+
 
 if __name__ == '__main__':
     print(("main"))
+
+    detectnoiselevel(3)
 
