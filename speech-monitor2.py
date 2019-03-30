96343dd252dc9383e964ee4f3b0f5851388297f5
ercagarwal <sharan.n21@gmail.com>
2019-03-30 15:10:48 +0530

    mod

--- speech-monitor2.py
+++ speech-monitor2.py
@@ -18,19 +18,20 @@ chunk_samples = int(fs * chunk_duration) # Each read length in number of samples
 DEFAULT_CHUNKNAME = './chunks/chunk{}.wav'
 
 frames = []
-writing = False
 
 
 subprocess.call('./remove-chunks.sh')
 
 fileOffset = getNumberOfFiles()
 
+run = True
+
+timestart = time.time()
+duration = 10
 
 
-q = Queue()
 
 
-# model = load_model('./models/tr_model.h5')
 
 
 def get_audio_input_stream(callback):
@@ -76,59 +77,28 @@ def callback(in_data, frame_count, time_info, status): # also responsible for pu
 
     # in_data is each chunk corresponding to 0.5 sec size
 
-    global run, timeout, data, silence_threshold, fileOffset, writing, frames
+    global run, duration, timestart
 
 
-
-    if time.time() > timeout: # stream will continue till timeout is invoked
+    if (time.time() - timestart) > duration: # stream will continue till timeout is invoked
         run = False
+
     data_new = np.frombuffer(in_data, dtype='int16')
+    frames.append(in_data)
 
 
     if np.abs(data_new).mean() < silence_threshold: # find the mean of the chunk for that small duration
         sys.stdout.write('-')
-        writing = False
-
-        if (writing == False) and (len(frames) != 0):  # if not writing and data is non empty, store in wav file
-
-            # get new filename
-            # print("saving")
-            filename = './samples/test' + str(fileOffset) + '.wav'
-            fileOffset = fileOffset + 1
-
-            storeWavFile(frames, filename, False)
-
-            #get mfcc of the file just saves and predict
-
-
-            # stream.stop_stream()
-
-            data = average(findMfcc(filename))
-            q.put(data)
-
-
-
-            # print(data)
-            # data = normalize(data)
-            # print(data)
-
-            # predictsingle(model, data)
-
-
-            frames = []
 
         return (in_data, pyaudio.paContinue)
 
     else:
-        # print('writing')
         sys.stdout.write('.')
-        frames.append(in_data)
-        writing = True
 
-
-    # splitWavFileAndStore(filename)
 
     return (in_data, pyaudio.paContinue)
+
+
 
 
 if __name__ == '__main__':
@@ -137,39 +107,23 @@ if __name__ == '__main__':
 
     model = loadmodel()
 
-    run = True
 
     silence_threshold = 100  # not in db
 
-    # Run the demo for a timeout seconds
-    timeout = time.time() + 2 * 60  # 2 minutes from now
 
-    # Data buffer for the input wavform
 
     stream = get_audio_input_stream(callback)  # creates the PyAudio() instance with callback function
-    # every time
     stream.start_stream()
 
-    try:
-        while True:
 
-            data = q.get()
-            n = normalize(data)
-            # print(n)
-            predictsingle(model,n)
+    while run:
+        continue
+
+    storeWavFile(frames, './sentences/testing.wav')
+
+    splitWavFileAndStore('./sentences/testing.wav')
 
 
-
-            # new_trigger = has_new_triggerword(preds, chunk_duration, feed_duration)
-            #
-            # if new_trigger:
-            #     sys.stdout.write('1')
-
-    except (KeyboardInterrupt, SystemExit):
-        stream.stop_stream()
-        stream.close()
-        timeout = time.time()
-        run = False
 
     stream.stop_stream()
     stream.close()
